d4617a737773c97a31304d3f6acbfb0725690536
Davidd0821 <126388661+Davidd0821@users.noreply.github.com>
2023-09-23 15:30:34 +0700

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeImageButton1_Click(object sender, System.EventArgs e)
+        {
+            ImgBtn1.Source = "new_img1.png";
+            Label1.Text = "Reality";
+        }
+
+        private void ChangeImageButton2_Click(object sender, System.EventArgs e) 
+        {
+            ImgBtn2.Source = "new_img2.png";
+            Label2.Text = "Reality";
+        }
+        }
+    }
